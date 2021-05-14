31cdceda4a87f9560caea41bb8792a599e9a97e1
youmangu <873211279@qq.com>
2021-05-14 11:04:49 +0800

    -添加服务器端文件

---
+++ Server/TreeWars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace TreeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, 88);
+            serverSocket.Bind(ipEndPoint);
+        }
+    }
+}
