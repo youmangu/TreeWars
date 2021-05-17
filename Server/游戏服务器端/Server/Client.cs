2a924d9949c3e21d0a1e783331724fb5788af8f8
youmangu <873211279@qq.com>
2021-05-17 18:49:42 +0800

    -创建服务类，用来进行客户端消息链接

---
+++ Server/游戏服务器端/Server/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+
+namespace GameServer.Server
+{
+    class Client
+    {
+        private Socket clientSocket;
+        private Server server;
+        private Message msg = new Message();
+
+        public Client() { }
+        public Client(Socket socket, Server server)
+        {
+            this.clientSocket = socket;
+            this.server = server;
+        }
+
+        public void Start()
+        {
+            clientSocket.BeginReceive(msg.Data, msg.StartIndex, msg.RemainSize, SocketFlags.None, ReceiveCallBack, null);
+        }
+
+        private void ReceiveCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                int count = clientSocket.EndReceive(ar);
+                if (count == 0)
+                {
+                    Close();
+                }
+
+                msg.ReadMessage(count);
+                Start();
+            }
+            catch (Exception e)
+            {
+                Close();
+            }
+
+            
+        }
+
+        private void Close()
+        {
+            if (clientSocket != null)
+                clientSocket.Close();
+            server.RemoveClient(this);
+        }
+    }
+}
---
+++ Server/游戏服务器端/Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+
+namespace GameServer.Server
+{
+    class Server
+    {
+        private IPEndPoint ipPendPoint;
+        private Socket serverSocket;
+        private List<Client> clientList = new List<Client>();
+
+        public Server() { }
+        public Server(string ipStr, int port)
+        {
+            SetIpAndPort(ipStr, port);
+        }
+
+        public void SetIpAndPort(string ipStr, int port)
+        {
+            ipPendPoint = new IPEndPoint(IPAddress.Parse(ipStr), port);
+        }
+
+        public void Start()
+        {
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(ipPendPoint);
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallBack, null);
+        }
+
+        private void AcceptCallBack(IAsyncResult ar)
+        {
+            Socket clientSocket = serverSocket.EndAccept(ar);
+            Client client = new Client(clientSocket, this);
+            client.Start();
+            clientList.Add(client);
+        }
+
+        public void RemoveClient(Client client)
+        {
+            lock (clientList)
+            {
+                clientList.Remove(client);
+            }
+            
+        }
+
+    }
+}
